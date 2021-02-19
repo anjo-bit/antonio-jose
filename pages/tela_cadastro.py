8db031bb65f9db4604d450931af83378b0bf456e
Antônio José <manoloco519@gmail.com>
2021-02-19 19:03:15 +0000

    verifica o erro do cadastro

--- pages/tela_cadastro.py
+++ pages/tela_cadastro.py
@@ -7,16 +7,20 @@ def telaCadastro():
   print("Bem vindo a melhor acaiteria da cidade!")
   print("--------------------------------------")
   print("Cadastre-se para continuar!")
-  senha = ""
-  nome = input("Digite seu nome: ")
-  telefone = input("Digite seu Número:")
-  email = input("Digite seu email: ")
-  while(True):
-    senha = input("Digite sua senha:")
-    confirmar_senha = input("Confirme sua senha:")
-    if(senha == confirmar_senha):
-      break
-    else:
-      os.system('cls' if os.name == 'nt' else 'clear')
-      print("As senhas não condizem digite novamente:/")
-  usuario.Usuario().registrarConta(nome, email, senha, telefone)
+  try:
+    senha = ""
+    nome = input("Digite seu nome: ")
+    telefone = input("Digite seu Número:")
+    email = input("Digite seu email: ")
+  except: 
+    print('alguma informaçao nao esta certa')
+  else:
+    while(True):
+      senha = input("Digite sua senha:")
+      confirmar_senha = input("Confirme sua senha:")
+      if(senha == confirmar_senha):
+        break
+      else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("As senhas não condizem digite novamente:/")
+    usuario.Usuario().registrarConta(nome, email, senha, telefone)
